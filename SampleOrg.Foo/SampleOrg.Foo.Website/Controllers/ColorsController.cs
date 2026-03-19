a2a2d2eddb5d2aedf8c81819729f6b6b39f681a9
Jeroen Heijmans <github@jeroenheijmans.nl>
2026-03-20 00:04:11 +0100

    Create cross-over page

--- SampleOrg.Foo/SampleOrg.Foo.Website/Controllers/ColorsController.cs
+++ SampleOrg.Foo/SampleOrg.Foo.Website/Controllers/ColorsController.cs
@@ -33,4 +33,32 @@ public class ColorsController : Controller
             .ToArray();
         return View((color, subjects));
     }
+
+    [HttpGet("{slug}/subjects/{id:int}")]
+    public IActionResult SubjectDetail(string slug, int id)
+    {
+        var color = AllColors.FirstOrDefault(c => c.Slug == slug);
+        if (color is null) return NotFound();
+
+        var subject = SubjectData.AllSubjects.FirstOrDefault(s => s.Id == id);
+        if (subject is null) return NotFound();
+
+        var roleIndex = Array.IndexOf(subject.ColorSlugs, slug);
+        if (roleIndex < 0) return NotFound();
+
+        var otherColors = subject.ColorSlugs
+            .Where(s => s != slug)
+            .Select(s => ColorData.BySlug.TryGetValue(s, out var c) ? c : null)
+            .Where(c => c is not null)
+            .Cast<ColorEntry>()
+            .ToArray();
+
+        var otherSubjects = SubjectData.AllSubjects
+            .Where(s => s.Id != id && s.ColorSlugs.Contains(slug))
+            .OrderBy(s => s.Name)
+            .Take(4)
+            .ToArray();
+
+        return View((color, subject, roleIndex, otherColors, otherSubjects));
+    }
 }
