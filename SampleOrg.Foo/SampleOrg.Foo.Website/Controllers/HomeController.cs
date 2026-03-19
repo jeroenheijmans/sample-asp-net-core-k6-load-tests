e34146fcff9a427d6b20c15360c799080de8791c
Jeroen Heijmans <github@jeroenheijmans.nl>
2026-03-19 22:30:46 +0100

    Add freshly scaffolded dotnet mvc app

---
+++ SampleOrg.Foo/SampleOrg.Foo.Website/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SampleOrg.Foo.Website.Models;
+
+namespace SampleOrg.Foo.Website.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
