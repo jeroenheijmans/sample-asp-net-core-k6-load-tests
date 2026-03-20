f86ecef0c82107dc3f3c425dfbf6bc1e262a66e1
Jeroen Heijmans <github@jeroenheijmans.nl>
2026-03-20 01:13:53 +0100

    Add several checks

--- SampleOrg.Foo.LoadTests/BaseLoadTest.ts
+++ SampleOrg.Foo.LoadTests/BaseLoadTest.ts
@@ -65,4 +65,34 @@ export default function (data: SetupData): void {
     'GET /colors status 200': (r) => r.status === 200,
     'GET /colors response time <= 1000ms': (r) => r.timings.duration <= 1000,
   });
+
+  const colorDetailResponse = http.get(`${config.baseDomain}/colors/red`);
+  check(colorDetailResponse, {
+    'GET /colors/red status 200': (r) => r.status === 200,
+    'GET /colors/red response time <= 1000ms': (r) => r.timings.duration <= 1000,
+  });
+
+  const colorSubjectsResponse = http.get(`${config.baseDomain}/colors/red/subjects`);
+  check(colorSubjectsResponse, {
+    'GET /colors/red/subjects status 200': (r) => r.status === 200,
+    'GET /colors/red/subjects response time <= 1000ms': (r) => r.timings.duration <= 1000,
+  });
+
+  const colorSubjectDetailResponse = http.get(`${config.baseDomain}/colors/red/subjects/1`);
+  check(colorSubjectDetailResponse, {
+    'GET /colors/red/subjects/1 status 200': (r) => r.status === 200,
+    'GET /colors/red/subjects/1 response time <= 1000ms': (r) => r.timings.duration <= 1000,
+  });
+
+  const subjectsResponse = http.get(`${config.baseDomain}/subjects`);
+  check(subjectsResponse, {
+    'GET /subjects status 200': (r) => r.status === 200,
+    'GET /subjects response time <= 1000ms': (r) => r.timings.duration <= 1000,
+  });
+
+  const subjectDetailResponse = http.get(`${config.baseDomain}/subjects/1`);
+  check(subjectDetailResponse, {
+    'GET /subjects/1 status 200': (r) => r.status === 200,
+    'GET /subjects/1 response time <= 1000ms': (r) => r.timings.duration <= 1000,
+  });
 }
